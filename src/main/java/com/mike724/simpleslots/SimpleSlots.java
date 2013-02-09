6ce85b6c2d763fb1d191035e93eb8d061add2792
Michael Meyer <michaelmeyer724@gmail.com>
2013-02-09 00:25:05 -0500

    Setup of base plugin

---
+++ src/main/java/com/mike724/simpleslots/SimpleSlots.java
@@ -0,0 +1,19 @@
+package com.mike724.simpleslots;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SimpleSlots extends JavaPlugin {
+
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		super.onDisable();
+	}
+
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		super.onEnable();
+	}
+
+}
