b181a700210147b209eba99b94fd023b0d140fd0
Dakota628 <dakota@dasigns.net>
2013-02-09 14:21:58 -0500

    Consistency

--- src/main/java/net/dasigns/commands/CommandHandler.java
+++ src/main/java/net/dasigns/commands/CommandHandler.java
@@ -43,7 +43,7 @@ public class CommandHandler implements CommandExecutor {
 			}
 		}
 		if(commandMethod == null) {
-			error(sender, "No sub-command provided");
+			error(sender, "no sub-command provided.");
 			return false;
 		}
 
