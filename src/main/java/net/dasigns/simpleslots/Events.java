5849a3e3f8db0f7efbb6dc20dd8524a12921bc78
Dakota628 <dakota@dasigns.net>
2013-02-09 22:17:41 -0500

    Started checking for lever pulled

--- src/main/java/net/dasigns/simpleslots/Events.java
+++ src/main/java/net/dasigns/simpleslots/Events.java
@@ -1,7 +1,22 @@
 package net.dasigns.simpleslots;
 
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 public class Events implements Listener {
 
+	@EventHandler
+	public void onPull(PlayerInteractEvent e) {
+		if(e.getAction() == Action.RIGHT_CLICK_BLOCK && e.getClickedBlock().getType() == Material.LEVER){
+			Block b = e.getClickedBlock();
+			e.getPlayer().sendMessage(Integer.toHexString(b.getState().getRawData() & 0x8));
+			if ((b.getState().getRawData() & 0x8) == 1) {
+				e.getPlayer().sendMessage("ON");
+			}
+		}
+	}
 }
