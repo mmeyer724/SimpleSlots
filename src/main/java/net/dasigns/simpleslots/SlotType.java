838036c4d53c539d4a9c9ceb79d6097182af499a
Dakota628 <dakota@dasigns.net>
2013-02-09 21:42:13 -0500

    Added SlotType class

---
+++ src/main/java/net/dasigns/simpleslots/SlotType.java
@@ -0,0 +1,55 @@
+package net.dasigns.simpleslots;
+
+import java.util.ArrayList;
+
+import org.bukkit.inventory.ItemStack;
+
+public class SlotType {
+	private Integer cost;
+	private Integer reward;
+	private Integer fireworks;
+	private ArrayList<String> cmds;
+	private WeightedRandomGenerator wrg;
+
+
+	public SlotType(String type) {
+		wrg = new WeightedRandomGenerator();
+		String typeNode = "types."+type;
+		String rewardNode = typeNode+".reward";
+
+		cost = Global.config.getInt(typeNode + ".cost");
+		reward = Global.config.getInt(rewardNode+".money");
+		fireworks = Global.config.getInt(rewardNode+".fireworks");
+		cmds = new ArrayList<String>(Global.config.getStringList(rewardNode+".cmd"));
+		
+		for(Integer w : Global.config.getIntegerList(typeNode+".items")) {
+			for(String s: Global.config.getStringList(typeNode+".items."+w)) {
+				String[] data = s.split(",");
+				ItemStack i = null;
+				if(data.length < 2) break;
+				if(data.length == 2) i = new ItemStack(Integer.parseInt(data[0]),1,Short.parseShort(data[1]));
+				wrg.put(i, w);
+			}
+		}
+	}
+
+	public Integer getCost() {
+		return cost;
+	}
+
+	public Integer getRewardMoney() {
+		return reward;
+	}
+
+	public ArrayList<String> getRewardCmds() {
+		return cmds;
+	}
+
+	public Integer getNumFireworks() {
+		return fireworks;
+	}
+
+	public ItemStack getNextItem() {
+		return (ItemStack) wrg.next();
+	}
+}
