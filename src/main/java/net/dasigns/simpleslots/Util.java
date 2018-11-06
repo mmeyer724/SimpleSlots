a80159912f8bff1d12e3b3f08b47948c7ce968e1
Boyana Kantarska <boianaradkova@gmail.com>
2018-11-06 12:11:49 +0200

    If statements simplification.

--- src/main/java/net/dasigns/simpleslots/Util.java
+++ src/main/java/net/dasigns/simpleslots/Util.java
@@ -6,23 +6,56 @@ import org.bukkit.Location;
 
 public class Util {
 	public static File createDir(File f) {
-		if(!f.exists()) f.mkdirs();
+		if(f.exists() == false) {
+			f.mkdirs();
+		}
+
 		return f;
 	}
 	
 	public static File createDir(String f) {
 		File fl = new File(f);
-		if(!fl.exists()) fl.mkdirs();
+
+		if(fl.exists() == false) {
+			fl.mkdirs();
+		}
+
 		return fl;
 	}
 	
 	public static Boolean locationEqualsXYZ(Location l, Integer x, Integer y, Integer z) {
-		if(l.getX() == x && l.getY() == y && l.getZ() == z) return true;
-		return false;
+		if(l.getX() != x) {
+			return false;
+		}
+
+		if(l.getY() != y) {
+			return false;
+		}
+
+		if(l.getZ() != z) {
+			return false;
+		}
+
+		return true;
 	}
 	
 	public static Boolean locationEqualsXYZW(Location l, Integer x, Integer y, Integer z,String w) {
-		if(l.getX() == x && l.getY() == y && l.getZ() == z && l.getWorld().getName() == w) return true;
-		return false;
+		if(l.getX() != x) {
+			return false;
+		}
+
+		if(l.getY() != y) {
+			return false;
+		}
+
+		if(l.getZ() != z) {
+			return false;
+		}
+
+		if(l.getWorld().getName() != w) {
+			return false;
+		}
+
+		return true;
 	}
 }
